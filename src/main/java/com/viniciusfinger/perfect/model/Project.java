01b83a1f5d29e84eea45b2a98bbae69ffa1e3913
Vinicius Finger <fingervinicius@icloud.com>
2024-01-14 00:11:27 -0300

    Creating project model

---
+++ src/main/java/com/viniciusfinger/perfect/model/Project.java
@@ -0,0 +1,22 @@
+package com.viniciusfinger.perfect.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+}
