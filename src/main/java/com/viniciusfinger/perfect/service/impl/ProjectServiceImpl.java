f4c6b82fd67e13d474a4752915f62b31660cac29
Vinicius Finger <fingervinicius@icloud.com>
2024-01-14 13:57:14 -0300

    Project CRUD

--- src/main/java/com/viniciusfinger/perfect/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/viniciusfinger/perfect/service/impl/ProjectServiceImpl.java
@@ -3,6 +3,10 @@ package com.viniciusfinger.perfect.service.impl;
 import com.viniciusfinger.perfect.model.Project;
 import com.viniciusfinger.perfect.repository.ProjectRepository;
 import com.viniciusfinger.perfect.service.ProjectService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +20,17 @@ public class ProjectServiceImpl implements ProjectService {
 
     public Project save(Project project){
         return this.projectRepository.save(project);
+    }
+
+    public void deleteById(Long id){
+        this.projectRepository.deleteById(id);
+    }
+
+    public Page<Project> findAll(int pageNo, int pageSize, String sortBy, String direction) {
+        Sort sort = Sort.by(Sort.Direction.fromString(direction), sortBy);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        return this.projectRepository.findAll(pageable);
     }
 
 }
